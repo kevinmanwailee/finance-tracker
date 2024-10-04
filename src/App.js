774698188c42c781d099b6860085fcd74576363c
Kevin Lee <kevin.manwai.lee@gmail.com>
2024-10-04 15:45:21 -0600

    Added current date as default value of add transaction

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import { Header } from './Header.js';
 import { Stack } from '@mui/material';
 import { Button, TextField, MenuItem } from '@mui/material';
 import { Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle } from '@mui/material';
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo'
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
 
 function NewTransaction(props) {
   const [open, setOpen] = React.useState(false);
@@ -72,14 +76,13 @@ function NewTransaction(props) {
                 label="Price"
                 type="number"
                 defaultValue="0.00"
-                sx={{ paddingRight:"30px" }}
-                />
-              <TextField
-                required
-                id="date"
-                name="date"
-                type="date"
+                sx={{ paddingRight:"30px", marginTop:"8px" }}
               />
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DatePicker', 'DatePicker']}>
+                  <DatePicker label="Date" defaultValue={dayjs()}/>
+                </DemoContainer>
+              </LocalizationProvider>
             </Stack>
             <Stack>
               <TextField
@@ -111,12 +114,9 @@ function NewTransaction(props) {
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [update, setUpdate] = useState(false);
-
   const URL = 'http://127.0.0.1:9000/';
 
   let fetchData = useCallback(async () =>{
-    setUpdate(false)
     const res = await axios.get(URL+"transactions");
     if(res){
       setPosts(res.data)
@@ -125,14 +125,14 @@ function App() {
 
   useEffect(() => {
     fetchData()
-  },[update])
+  },[])
 
   function newPost(jsonObj){
     console.log("Sending: " + jsonObj)
     axios.put(URL+"AddTransaction/", jsonObj)
       .then((response) => {
         console.log(response.data)
-        setUpdate(true)
+        fetchData()
     })
       .catch((error) => {
         console.log(error)
